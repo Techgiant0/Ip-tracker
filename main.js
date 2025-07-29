c9246599d3f99d254fea15315e4f3371fe91400d
Techgiant <simonsamuel195@gmail.com>
2025-07-29 16:13:48 +0100

    added tab switching

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 const ipToggle = document.getElementById('ip')
 const phoneToggle = document.getElementById('phone')
+const ipInput = document.getElementsByClassName('ip-input-section')[0]
+const phoneInput = document.getElementsByClassName('phone-input-section')[0]
+
+phoneToggle.addEventListener('click',()=>{
+    phoneToggle.classList.add('selected')
+    ipToggle.classList.remove('selected')
+    ipInput.classList.add('hidden')
+    phoneInput.classList.remove('hidden')
+})
+
+ipToggle.addEventListener('click',()=>{
+    ipToggle.classList.add('selected')
+    phoneToggle.classList.remove('selected')
+    phoneInput.classList.add('hidden')
+    ipInput.classList.remove('hidden')
+})
