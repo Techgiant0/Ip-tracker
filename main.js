c7037d67a6ffcfa29f2d6176b120c3310748711c
Techgiant <simonsamuel195@gmail.com>
2025-07-30 14:16:31 +0100

    completed tab switching logic

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ const ipToggle = document.getElementById('ip')
 const phoneToggle = document.getElementById('phone')
 const ipInput = document.getElementsByClassName('ip-input-section')[0]
 const phoneInput = document.getElementsByClassName('phone-input-section')[0]
+const ipSection = document.querySelector('.ip-section')
+const phoneSection = document.querySelector('.phone-section')
 
 phoneToggle.addEventListener('click',()=>{
     phoneToggle.classList.add('selected')
     ipToggle.classList.remove('selected')
     ipInput.classList.add('hidden')
     phoneInput.classList.remove('hidden')
+    ipSection.classList.add('hidden')
+    phoneSection.classList.remove('hidden')
 })
 
 ipToggle.addEventListener('click',()=>{
@@ -15,4 +19,6 @@ ipToggle.addEventListener('click',()=>{
     phoneToggle.classList.remove('selected')
     phoneInput.classList.add('hidden')
     ipInput.classList.remove('hidden')
+    ipSection.classList.remove('hidden')
+    phoneSection.classList.add('hidden')
 })
