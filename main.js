298d70f7812a9a073915d233080bbdf942d0e9ad
Techgiant <simonsamuel195@gmail.com>
2025-07-31 02:42:40 +0100

    added show more feature

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const phoneInput = document.getElementsByClassName('phone-input-section')[0]
 const ipSection = document.querySelector('.ip-section')
 const phoneSection = document.querySelector('.phone-section')
 const map = document.getElementsByClassName('map-section')[0]
+const lessInfo = document.getElementById('less-info')
+const moreInfo = document.getElementById('more-info')
+const grp = document.getElementById('grp2')
 
 phoneToggle.addEventListener('click',()=>{
     phoneToggle.classList.add('selected')
@@ -24,4 +27,16 @@ ipToggle.addEventListener('click',()=>{
     ipSection.classList.remove('hidden')
     phoneSection.classList.add('hidden')
     map.classList.remove('hidden')
+})
+
+moreInfo.addEventListener('click', ()=>{
+    grp.classList.remove('hidden')
+    moreInfo.classList.add('hidden')
+    lessInfo.classList.remove('hidden')
+})
+
+lessInfo.addEventListener('click', ()=>{
+    grp.classList.add('hidden')
+    moreInfo.classList.remove('hidden')
+    lessInfo.classList.add('hidden')
 })
